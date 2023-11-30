61f777f749d385dbe2bc0faa16fb7cf8d2e3f538
drcatjk <jakobbk3@gmail.com>
2023-11-30 11:24:33 +0100

    fixed different redirect for different login. Vulnerability (that may be acceptable in showcase of product) is ability to cross between users by changing url

--- code/app.py
+++ code/app.py
@@ -3,10 +3,22 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # Dummy user data (replace this with a database in a real application)
-users = {
-    'user1': 'password1',
-    'user2': 'password2'
+customers = {
+    'user1': 'u1',
+    'user2': 'u2'
 }
+
+serviceproviders = {
+    "samhall1": "s1",
+    "samhall2": "s2"
+}
+
+manufacturers = {
+    'husqvarna1': 'h1',
+    'husqvarna2': 'h2'
+}
+
+
 
 @app.route('/')
 def index():
@@ -17,16 +29,32 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    if username in users and users[username] == password:
-        # Successful login, redirect to a different page
-        return redirect(url_for('success'))
+    # Successful login, redirect to a different page
+    if username in customers and customers[username] == password:
+        return redirect(url_for("customer"))
+    elif username in serviceproviders and serviceproviders[username] == password:
+        return redirect(url_for("serviceprovider"))
+    elif username in manufacturers and manufacturers[username] == password:
+        return redirect(url_for("manufacturer"))
+    
+    # Failed login, redirect back to the login page with an error message
     else:
-        # Failed login, redirect back to the login page with an error message
         return render_template('login.html', error='Invalid username or password')
 
-@app.route('/success')
-def success():
-    return 'Login successful!'
+@app.route("/customer")
+def customer():
+    return render_template("CusMain.html")
+
+@app.route("/serviceprovider")
+def serviceprovider():
+    return render_template("SePrMain.html")
+    
+
+@app.route("/manufacturer")
+def manufacturer():
+    return render_template("ManMain.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
