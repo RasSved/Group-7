ceed5915a9a3a51c65e40e56d971ddb8e34ffd6d
Nils Borg <nils.ml.borg@gmail.com>
2024-01-18 14:53:15 +0100

    nav functionality

--- code/routes/customer.py
+++ code/routes/customer.py
@@ -30,16 +30,76 @@ def addArea():
             areas.insert_one({"Sub": sub, "grassLength": grassLength, "CustomerId": 0, "ServiceId": None, "Address": address})      
     return redirect(url_for("customer.customer"))
 
-@customer_bp.route("/area", methods = ["GET", "POST"])
-def area():
+@customer_bp.route("/enter", methods = ["GET", "POST"])
+def enterArea():
     print(request.form, file=sys.stderr)
     if "areaId" in request.form:
         areaId = request.form["areaId"]
-        area = areas.find({"_id": ObjectId(areaId)})[0]    # find area where id is the same as area clicked
+        # find area where id is the same as area clicked
         #print(area, file=sys.stderr)
-        return render_template("CusArea.html", area=area)
+        session["area_id"] = areaId
+        return redirect(url_for("customer.area"))
     else:
         return redirect(url_for("customer.customer"))
 
+@customer_bp.route("/area", methods = ["GET", "POST"])
+def area():
+    if "area_id" in session:
+        areaId = session["area_id"]
+        area = areas.find({"_id": ObjectId(areaId)})[0]    # find area where id is the same as area clicked
+        return render_template("CusArea.html", area=area)
+    else:
+        return redirect(url_for("customer.customer"))
+    
+
+@customer_bp.route("/area/nav", methods = ["GET", "POST"])  # Depending on which button was clicked from navigation form, navigate
+def areaNav():
+    print(request.form, file=sys.stderr)
+    page = request.form["navBar"]
+    if page == "map":
+        return redirect(url_for("customer.map"))
+    elif page == "schedule":
+        return redirect(url_for("customer.schedule"))
+    elif page == "settings":
+        return redirect(url_for("customer.settings"))
+    elif page == "home":
+        return redirect(url_for("customer.area"))
+    else:
+        return redirect(url_for("customer.customer"))
+
+@customer_bp.route("/area/map", methods = ["GET", "POST"])
+def map():
+    print(request.form, file=sys.stderr)
+    if "area_id" in session:
+        areaId = session["area_id"]
+        area = areas.find({"_id": ObjectId(areaId)})[0]    # find area where id is the same as area clicked
+        #print(area, file=sys.stderr)
+        return render_template("CusSett.html", area=area)
+    else:
+        return redirect(url_for("customer.customer"))
+
+@customer_bp.route("/area/schedule", methods = ["GET", "POST"])
+def schedule():
+    print(request.form, file=sys.stderr)
+    if "area_id" in session:
+        areaId = session["area_id"]
+        area = areas.find({"_id": ObjectId(areaId)})[0]    # find area where id is the same as area clicked
+        #print(area, file=sys.stderr)
+        return render_template("CusSched.html", area=area)
+    else:
+        return redirect(url_for("customer.customer"))
+
+@customer_bp.route("/area/settings", methods = ["GET", "POST"])
+def settings():
+    print(request.form, file=sys.stderr)
+    if "area_id" in session:
+        areaId = session["area_id"]
+        area = areas.find({"_id": ObjectId(areaId)})[0]    # find area where id is the same as area clicked
+        #print(area, file=sys.stderr)
+        return render_template("CusSett.html", area=area)
+    else:
+        return redirect(url_for("customer.customer"))
+    
+    
 
 
